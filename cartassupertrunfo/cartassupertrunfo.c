453d6d4058ff555bb51f2f42dc5d5b11b4ace549
SilvaZim <ls0457809@gmail.com>
2025-03-27 01:36:16 +0000

    cartassupertrunfo

---
+++ cartassupertrunfo/cartassupertrunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int carta01;
+    int cidade[20];
+    int População[20], Números_pontos_turisticos[100]; 
+    char estado, codígo;
+    float Áreakm2, PIB;
+    char 
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    return 0;
+}
